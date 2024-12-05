f79c8c4cfa524134a55e8693f3e4fdeb04e3dc8f
ayushchauhan7 <chauhan0007ayush@gmail.com>
2024-12-05 09:46:33 +0530

    second commit.

--- Day_10/hooks-react-app/src/components/CounterApp.jsx
+++ Day_10/hooks-react-app/src/components/CounterApp.jsx
@@ -7,7 +7,7 @@ const CounterApp = () => {
         <div className="disp">
             <h1 className="header">    {count}</h1>
             <button className="btn" onClick={() => {setCount(count + 1);}}>Increment</button>
-            <button className="btn">Reset</button>
+            <button className="btn" onClick={() => {setCount(0);}}>Reset</button>
             <button className="btn" onClick={() => {setCount(count - 1);}}>Decrement</button>
         </div>
     );
