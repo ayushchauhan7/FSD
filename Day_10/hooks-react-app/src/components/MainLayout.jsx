57ae213a6fbf284249b8b083e8dea3714065d91a
ayushchauhan7 <chauhan0007ayush@gmail.com>
2024-12-06 14:47:35 +0530

    second commit.

--- Day_10/hooks-react-app/src/components/MainLayout.jsx
+++ Day_10/hooks-react-app/src/components/MainLayout.jsx
@@ -1,9 +1,22 @@
     import React from 'react'
+    import {Link, Outlet} from "react-router-dom";
 
     const MainLayout = () => {
         return (
-            <div>MainLayout</div>
-        )
-    }
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <link to="/counterapp">Counter App</link>
+                        </li>
+                        <li>
+                            <link to="/imageapp">Image App</link>
+                        </li>
+                    </ul>
+                </nav>
+                <Outlet />
+            </div>
+        );
+    };
 
-    export default MainLayout
+    export default MainLayout;
